849270ccf2b78db6fe7dd065fa21557fc9072759
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:45:14 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"This is my car. Do you like ___ .",
+        options:["him", "it", "her"],
+        answer: 1
+    },
+    {
+     q:"She is so beautiful! Look at ___ .",
+        options:["her", "she", "me"],
+        answer: 0   
+    },
+    {
+        q:"I never drink coffee. I hate ___ .",
+        options:["them", "him", "it"],
+        answer: 2
+    },
+    {
+        q:"She doesn't like dogs. She is afraid of ___ . ",
+        options:[ "her", "it", "them"],
+        answer: 2
+    },
+    {
+        q:"Mark is a very clever man. Do you know ___ ?",
+        options:["him", "he", "it",],
+        answer: 0
+    },
+    {
+        q:"I don't need this book. You can take ___ .",
+        options:["her", "it", "them"],
+        answer: 1
+    },
+    {
+        q:"Where are my keys? I can't find ___ ?",
+        options:["them", "they", "it"],
+        answer: 0
+    },
+    {
+        q:"What's the matter with ___ ? You look bad.",
+        options:["me", "you", "us"],
+        answer: 1
+    },
+    {
+        q:"I'll be back in a minute. Wait for ___ , please.",
+        options:["you", "me", "it"],
+        answer: 1
+    },
+    {
+        q:"We are going to the cinema. Do you want to join ___ ?",
+        options:["them", "you", "us"],
+        answer: 2
+    }
+
+]
